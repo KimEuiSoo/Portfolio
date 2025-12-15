1fe8bfc6da28fddd5d80247f5f187009ca1ec8a0
BOOK-1V2KOUUPE2\dmltn <dmltn3426@naver.com>
2025-12-15 20:35:00 +0900

    design: profile, career 디자인 template 통합 가능한것 통합

--- portfolio/src/components/templates/home/HomeTemplate.tsx
+++ portfolio/src/components/templates/home/HomeTemplate.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import clsN from 'classnames';
 import Profile from '../../organisms/Profile';
 import Career from '../../organisms/Career';
-import styles from './styles/HomeTemplate.module.scss'
+import styles from './styles/HomeTemplate.module.scss';
 
 const HomeTemplate = () => {
     return (
-        <div className={clsN(styles["home-container"])}>
-            <Profile />
-            <div className={clsN(styles["home-container__career"])}>
-                <Career/>
+        <div className={clsN(styles['home-container'])}>
+            <div className={clsN(styles['home-container__profile'])}>
+                <Profile />
             </div>
+            <section className={clsN(styles['home-container__careerSection'])}>
+                <div className={clsN(styles['home-container__career'])}>
+                    <Career />
+                </div>
+            </section>
         </div>
-    )
-}
+    );
+};
 
 export default HomeTemplate;
